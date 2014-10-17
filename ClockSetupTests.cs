d211f670cdf5017bafb584a56d91a6868c6cfccc
Sebastian Forss <sebastianforss@hotmail.com>
2014-10-17 11:04:18 +0200

    Add VS project

---
+++ ClockSetupTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StateTestsLib
+{
+    [TestClass]
+    public class ClockSetupTests
+    {
+        [TestMethod]
+        public void Test_That_ClockSetup_Returns_Correct_Date()
+        {
+            // Arrange
+            // The initial date in ClockSetup should be 15th march 2011
+            DateTime expectedDate = new DateTime(2012, 4, 14, 0, 0, 0);
+            ClockSetup setup = new ClockSetup();
+            setup.YearState = new YearState(setup, 2011);
+            setup.MonthState = new MonthState(setup, 3);
+            setup.DayState = new DayState(setup, 15);
+            // Act
+            setup.PushKnob();
+            setup.RotateRight();
+            setup.PushKnob();
+            setup.RotateRight();
+            setup.PushKnob();
+            setup.RotateLeft();
+            setup.PushKnob();
+            // Assert
+            Assert.AreEqual(expectedDate, setup.GetSelectedDate());
+        }
+    }
+}
---
+++ StatePattern/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    public class Class1
+    {
+    }
+}
